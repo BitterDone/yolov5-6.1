e753a89a99edfddb3fbecb99acc9d4a6476aae6d
hdb <hdb@gmail.com>
2025-11-10 14:54:10 +0000

    add --weights ''

--- bare-vm-setup.sh
+++ bare-vm-setup.sh
@@ -16,13 +16,13 @@ echo 'export PYENV_ROOT="$HOME/.pyenv"' >> ~/.profile
 echo '[[ -d $PYENV_ROOT/bin ]] && export PATH="$PYENV_ROOT/bin:$PATH"' >> ~/.profile
 echo 'eval "$(pyenv init - bash)"' >> ~/.profile
 
-# Reloads the shell with new env vars
-exec "$SHELL"
+# # Reloads the shell with new env vars
+# exec "$SHELL"
 
-# # Try these next time
-# source ~/.bashrc
-# source ~/.bash_profile
-# source ~/.profile
+# Try these next time
+source ~/.bashrc
+source ~/.bash_profile
+source ~/.profile
 
 # Install modules for pyenv install 3.9.0
 sudo apt update && sudo apt install -y \
@@ -31,26 +31,26 @@ sudo apt update && sudo apt install -y \
   libncursesw5-dev xz-utils tk-dev \
   libffi-dev liblzma-dev python3-openssl git curl
 
-# pyenv install 3.9.0 
-# pyenv global 3.9.0
-# pyenv virtualenv 3.9.0 yolov5-env
-# # Error about not having env vars present in the shell?
-# pyenv activate yolov5-env
+pyenv install 3.9.0 
+pyenv global 3.9.0
+pyenv virtualenv 3.9.0 yolov5-env
+# Error about not having env vars present in the shell?
+pyenv activate yolov5-env
 
-# pip install -r requirements.txt
+pip install -r requirements.txt
 
-# pip uninstall torch torchvision torchaudio -y
-# pip cache purge
-# pip install networkx==3.1
-# pip install torch==2.2.0+cu118 torchvision==0.17.1+cu118 torchaudio==2.3.1+cu118 --extra-index-url https://download.pytorch.org/whl/cu118
+pip uninstall torch torchvision torchaudio -y
+pip cache purge
+pip install networkx==3.1
+pip install torch==2.2.0+cu118 torchvision==0.17.1+cu118 torchaudio==2.3.1+cu118 --extra-index-url https://download.pytorch.org/whl/cu118
 
 
-# python train.py --img 640 --batch 16 --epochs 100 --data railroad-cars/data.yaml --name railroad_detector
-# python train.py --img 640 --batch 16 --epochs 300 --data railroad-cars/data.yaml --name railroad_detector
-# python train.py --img 640 --batch 16 --epochs 600 --data railroad-cars/data.yaml --name railroad_detector
-# python train.py --img 640 --batch 16 --epochs 100 --data railroad-cars/data.yaml --name railroad_detector_aug --image-weights  --hyp data/hyps/hyp.scratch-custom.yaml
-# python train.py --img 640 --batch 16 --epochs 300 --data railroad-cars/data.yaml --name railroad_detector_aug --image-weights  --hyp data/hyps/hyp.scratch-custom.yaml
-# python train.py --img 640 --batch 16 --epochs 600 --data railroad-cars/data.yaml --name railroad_detector_aug --image-weights  --hyp data/hyps/hyp.scratch-custom.yaml
+# python train.py --img 640 --batch 16 --epochs 100 --weights "" --data railroad-cars/data.yaml --name railroad_detector
+# python train.py --img 640 --batch 16 --epochs 300 --weights "" --data railroad-cars/data.yaml --name railroad_detector
+# python train.py --img 640 --batch 16 --epochs 600 --weights "" --data railroad-cars/data.yaml --name railroad_detector
+# python train.py --img 640 --batch 16 --epochs 100 --weights "" --data railroad-cars/data.yaml --name railroad_detector_aug --image-weights  --hyp data/hyps/hyp.scratch-custom.yaml
+# python train.py --img 640 --batch 16 --epochs 300 --weights "" --data railroad-cars/data.yaml --name railroad_detector_aug --image-weights  --hyp data/hyps/hyp.scratch-custom.yaml
+# python train.py --img 640 --batch 16 --epochs 600 --weights "" --data railroad-cars/data.yaml --name railroad_detector_aug --image-weights  --hyp data/hyps/hyp.scratch-custom.yaml
 
 # python val.py --weights runs/train/railroad_detector/weights/best.pt --data railroad-cars/data.yaml --img 640 --batch 16
 
