768763e9c9d43498b8c046ecf42e369b6e357fbc
Dan Bitter <bitter.daniel@gmail.com>
2025-12-07 15:06:01 -0500

    Move debugging script to a separate file

--- pi-root/project-scripts/detect_stream.py
+++ pi-root/project-scripts/detect_stream.py
@@ -8,6 +8,8 @@ import onnxruntime as ort
 from pathlib import Path
 import os
 
+from debugging import debug
+
 print(f"Loading config")
 # --------------------------------------------------------
 # Load config
@@ -129,29 +131,7 @@ print("Frame read:", ok, flush=True)
 
 print("Streaming started. Running detection...", flush=True)
 
-
-print("Begin debugging script", flush=True)
-print("cwd =", os.getcwd(), flush=True)
-frame = cv2.imread("/home/danbitter/yolov5-6.1/pi-root/project-scripts/freight_train.jpg")
-print("frame is None?", frame is None, flush=True)
-
-img = preprocess(frame)
-
-# input_name must match your model’s input tensor name. You can print it using:
-input_name = session.get_inputs()[0].name
-print("input_name to match model input tensor name", input_name, flush=True) # images
-outputs = session.run(None, {input_name: img})
-
-# Print the raw ONNX output shape(s)
-print("Number of outputs:", len(outputs), flush=True)
-for i, out in enumerate(outputs):
-    print(f"Output[{i}] shape:", out.shape, flush=True)
-    # For safety, only print the first few rows
-    print(f"Output[{i}] sample:", out.reshape(-1, out.shape[-1])[:5], flush=True)
-
-
-conf, class_ids = parse_output(outputs)
-print("parse_output: ", conf, class_ids, flush=True)
+debug(os, cv2, preprocess, session, parse_output)
 
 
 # # --------------------------------------------------------
